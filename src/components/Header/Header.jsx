9f352a5f94e4e239891c9935b8d6190de53ed036
thgiolito <thgiolito@gmail.com>
2021-11-25 12:08:38 +0100

    first commit header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
+import Logo from "../../assets/Run_Time.png";
 
 export default function Header() {
     return (
         <div className={styles.container}>
-            <h2>Salut, je suis le Header</h2>
+            <img src={Logo} />
+            <h2>PlaceHolder</h2>
+
         </div>
     )
 }
