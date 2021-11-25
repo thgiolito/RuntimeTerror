bcf5f96558cf9f941100649f0bdeb1e3773b4a17
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-11-25 14:48:03 +0100

    Fucking styling

--- src/screens/Score/Score.jsx
+++ src/screens/Score/Score.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import styles from "./Score.module.css";
 
 export default function Score() {
+
+
   return (
     <div className={styles.container}>
       <h2>Salut, je suis le score</h2>
