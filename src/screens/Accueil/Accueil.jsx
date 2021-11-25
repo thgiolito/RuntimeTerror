64a47ea730af75e03abb3ae88f461852f8f1955c
Your Name <you@example.com>
2021-11-25 16:33:22 +0100

    add Paragraph in Accueil

--- src/screens/Accueil/Accueil.jsx
+++ src/screens/Accueil/Accueil.jsx
@@ -1,12 +1,22 @@
 import { Link } from "react-router-dom";
 import Webcam from "react-webcam";
+import Logo from "../../assets/Run_Time.png";
+
 
 import styles from "./Accueil.module.css";
 
 export default function Accueil() {
   return (
     <div className={styles.container}>
-      <h2>Welcome !</h2>
+      <h2>Welcome to RUNTIME TERROR !</h2>
+      <p>RUNTIME TERROR it's a music rhythm video game. The game features a hand-shaped controller that the player uses to simulate playing and dancing music.</p>
+      <img src={Logo} alt="logo" className={styles.logo} />
+
+      <h3> BEFORE STARTING TO PLAY...</h3>
+      <p> Be wary of you environment, establish safe distances and take a few deep breathes! </p>
+      
+      <h3> Are you ready to dance Music? </h3>
+
       <h3>Make sure your camera works</h3>
       <div className={styles.videoContainer}>
         <Webcam audio={false} height={375} width={500} />
