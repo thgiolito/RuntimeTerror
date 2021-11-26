f63c164b6e5bf51e32e51d4519d743b1239e9be8
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-11-26 10:53:46 +0100

    style

--- src/screens/Accueil/Accueil.jsx
+++ src/screens/Accueil/Accueil.jsx
@@ -5,13 +5,12 @@ import Webcam from "react-webcam";
 import UserContext from "../../contexts/usercontext";
 
 import Logo from "../../assets/Run_Time.png";
-
+import Reload from "../../assets/reload.png";
 
 import styles from "./Accueil.module.css";
 
 export default function Accueil() {
-
-  const { user, setUser, setChore } = useContext(UserContext)
+  const { user, setUser, setChore } = useContext(UserContext);
 
   useEffect(() => {
     setChore([
@@ -26,39 +25,57 @@ export default function Accueil() {
 
   const handleNameChange = (e) => {
     setUser(e.target.value);
-  }
-  
+  };
+
   return (
     <div className={styles.main}>
       <div className={styles.jukebox}>
         <div className={styles.containerText}>
-          <h2>Welcome to RUNTIME TERROR !</h2>
-          <p>RUNTIME TERROR is a fresh, simple dancing game. You simply need a webcam, and a little bit of coordination !</p>
-          <h4> Good luck {user} ! </h4>
+          <p className={styles.introText}>
+            RUNTIME TERROR is a fresh, simple dancing game.
+          </p>
+          <p className={styles.introText}>
+            You only need a webcam... and a little bit of coordination !
+          </p>
+          <p className={styles.introText}>
+            <b>Good luck{user ? ", " + user : ""} !</b>
+          </p>
         </div>
-        <div className={styles.container} id="LogoHome" >
+        <div className={styles.container} id="LogoHome">
           <img src={Logo} alt="logo" className={styles.logo} />
         </div>
         <div className={styles.containerOptions} id="start">
           <h3> BEFORE STARTING TO PLAY...</h3>
-          <p> Be wary of your environment, establish safe distances and take a few deep breathes! </p>
+          <p>
+            {" "}
+            Be wary of your environment, establish safe distances and take a few
+            deep breathes!{" "}
+          </p>
         </div>
         <div className={styles.containerForm}>
-        <form>
-          <input className={styles.nameForm} onChange={handleNameChange} type="text" placeholder="Enter your name!"></input>
-        </form>
+          <form>
+            <input
+              className={styles.nameForm}
+              onChange={handleNameChange}
+              type="text"
+              placeholder="Enter your name!"
+            ></input>
+          </form>
+        </div>
+        <div className={styles.precamcontainer} id="camera">
+          <p>Make sure that your camera works </p>
+          <p> and you can touch the borders</p>
+        </div>
+
+        <div className={styles.videoContainer}>
+          {/* <div className={styles.webcamoff}></div> */}
+          <Webcam audio={false} height={375} width={500} zindex={91} />
+        </div>
+
+        <Link to="/game">
+          <button className={styles.gameButton}>New Game</button>
+        </Link>
       </div>
-      <div className={styles.container} id="camera">
-      <h3>Welcome{user ? ', ' + user : '' }, make sure your camera works</h3>
-      </div>
-      <div className={styles.videoContainer}>
-        <Webcam audio={false} height={375} width={500} />
-      </div>
-      <Link to="/game">
-        <button className={styles.gameButton}>New Game</button>
-      </Link>
     </div>
-  </div>
-  
   );
 }
